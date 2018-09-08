44ec92054a5175f54459c83b53eb20d51ef21f27
akm341 <43067928+akm341@users.noreply.github.com>
2018-09-08 10:24:02 +0200

    new

--- array_list.py
+++ array_list.py
@@ -2,3 +2,13 @@ friends = ["Deepak", "Soumya", "Sameer"]
 roll = [123,502,377]
 print (friends[0:])
 print (roll[0:])
+
+print("\n\n")
+
+is_gay = True
+is_friend = False
+
+if is_gay or is_friend:
+    print("They are friend and gay")
+else:
+    print("Fucker neither friend nor gay")
